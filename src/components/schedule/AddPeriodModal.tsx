77b0f35b540940325b912ac466ed766203baea47
Samuel Riascos Prieto <samuel.riascos@correounivalle.edu.co>
2024-04-16 02:38:14 -0500

    Se termina la página de configuración de agenda del practicantes
    1- Se crea un botón en cada celda de la tabla
    2- Al presionar cada botón, este alterna entre "disponible" o "no disponible", aplicando las sugerencias del director
    3- Se crea un botón "Cancelar cambios" para deshacer los cambios
    4- Se crea un botón "Guardar cambios" para guardar los cambios desde la página y no desde la modal
    5- Se crea un tema exclusivo para la página
    6- Se crea el archivo "constants.ts", destinado a almacenar distintas constantes recurrentes
    7- Se modulariza la página, dividiendo el código en distintos archivos de componentes

---
+++ src/components/schedule/AddPeriodModal.tsx
@@ -0,0 +1,66 @@
+"use client"
+import ModalStates from "@/enums/PeriodModalStates";
+import { IDay } from "@/interfaces/schedule/IDay";
+import { days, hours } from "@/utils/constants";
+import { Dialog, Box, InputLabel, Select, MenuItem, Stack, Button } from "@mui/material";
+import { useSession } from "next-auth/react";
+
+export default function AddPeriodModal({schedule, setSchedule, state, close} : 
+    {
+        schedule: IDay[], 
+        setSchedule: React.Dispatch<React.SetStateAction<IDay[]>>,
+        state: ModalStates,
+        close: ()=>void
+    }) {
+        const {data: session} = useSession();
+        let dia: IDay["day"];
+        let horaInicial: number;
+        let horaFinal: number;
+        // console.log("el estado de la modal es:" + state);
+
+        function handleUpdate() {
+            const i = days.indexOf(dia);
+            for (let j = horaInicial; j <= horaFinal; j++) {
+                // schedule.hours[i] = true;
+                schedule[i].hours[j] = state === ModalStates.Add ? true : false;
+            }
+            // console.log("Ahora las horas son: " + schedule.hours);
+            setSchedule([...schedule]);
+            close();
+        }
+        return(
+            <Dialog
+                open={state !== ModalStates.Closed}
+                onClose={close}
+            >
+                <Box padding={2}>
+                <InputLabel id="dia">Día:</InputLabel>
+                <Select labelId="dia" onChange={(e) => {dia = e.target.value as IDay["day"]}} fullWidth>
+                    {days.map((day) => (
+                        <MenuItem key={`opcion${day}`} value={day}>{day}</MenuItem>
+                    ))}
+                </Select>
+                <InputLabel id="horaInicial">Desde las:</InputLabel>
+                <Select labelId="horaInicial" onChange={(e) => {horaInicial = e.target.value as number}} fullWidth>
+                    {hours.map((hour) => 
+                        <MenuItem key={`opcion${hour}inicial`} value={hour.getValue()}>{hour.getString()}</MenuItem>
+                    )}
+                </Select>
+                <InputLabel id="horaFinal">Hasta las:</InputLabel>
+                <Select labelId="horaFinal" onChange={(e) => {horaFinal = e.target.value as number}} fullWidth>
+                {hours.map((hour) => 
+                        <MenuItem key={`opcion${hour}final`} value={hour.getValue()}>{hour.getString()}</MenuItem>
+                    )}
+                </Select>
+                <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" padding={2}>
+                    <Button size="large" color={state === ModalStates.Add ? "secondary" : "error"} onClick={handleUpdate}>
+                        {state !== ModalStates.Remove ? "Agregar período" : "Eliminar período"}
+                    </Button>
+                    <Button onClick={close}>
+                        Cancelar
+                    </Button>
+                </Stack>
+                </Box>
+            </Dialog>
+        )
+    }
---
+++ src/themes/schedule-theme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+import { blueGrey, purple } from "@mui/material/colors";
+
+export const scheduleTheme = createTheme({
+    palette: {
+        primary: purple,
+        secondary: blueGrey,
+      },
+})
