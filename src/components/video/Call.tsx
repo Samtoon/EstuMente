ea0ff4d64919e086f7d1ec4fec9b1cda693ecb6c
Samuel Riascos Prieto <samuel.riascos@correounivalle.edu.co>
2024-04-23 23:52:19 -0500

    Se carga el nombre del usuario logueado en la llamada y se habilita la opción de reingresar a la llamada cuando se sale de ésta

--- src/components/video/Call.tsx
+++ src/components/video/Call.tsx
@@ -2,6 +2,7 @@ import { FC, useCallback, useEffect, useRef } from "react";
 // import { psiApi } from "../../axios-api";
 import DailyIframe from "@daily-co/daily-js";
 import { Box } from "@mui/material";
+import { useSession } from "next-auth/react";
 
 interface Props {
   room: any;
@@ -25,76 +26,75 @@ export const Call: FC<Props> = ({
   const handleUpdateChecking = async () => {
     // await psiApi.put("/appointments", { id: appointmentId });
   };
-
+  const {data: session, status} = useSession();
   const callRef = useRef<any>(null);
 
-  const createAndJoinCall = useCallback(async () => {
-    console.log("Creando nuevo objeto call");
-    try {
-      const oldCall = DailyIframe.getCallInstance();
-      if (oldCall) {
-        // console.log("Sí existe el bicho");
-        await oldCall.destroy();
-      }
-      
-    } catch (error) {
-      console.log("Hubo un error: " + error);
-    }
-    const newCallFrame = DailyIframe.createFrame(callRef?.current, {
-      iframeStyle: {
-        height: "calc(100vh - 67px)",
-        width: "100%",
-        aspectRatio: "16 / 9",
-        minHeight: "calc(100vh - 67px)",
-        border: "0",
-      },
-      theme: {
-        colors: {
-          accent: "#4D22B3",
-          accentText: "#FFFFFF",
-          background: "#1F1D36",
-          backgroundAccent: "#311673",
-          baseText: "#FFFFFF",
-          border: "#574770",
-          mainAreaBg: "#000000",
-          mainAreaBgAccent: "#333333",
-          mainAreaText: "#FFFFFF",
-          supportiveText: "#808080",
+  const createAndJoinCall = async () => {
+    const oldCall = DailyIframe.getCallInstance();
+    if(!oldCall) {
+      console.log("Creando nuevo objeto call");
+      const newCallFrame = DailyIframe.createFrame(callRef?.current, {
+        iframeStyle: {
+          height: "calc(100vh - 67px)",
+          width: "100%",
+          aspectRatio: "16 / 9",
+          minHeight: "calc(100vh - 67px)",
+          border: "0",
         },
-      },
-      userName: userName,
-      showLeaveButton: true,
-      showFullscreenButton: true,
-    });
-    console.log("callFrame Creado, es: " + newCallFrame);
-    setCallFrame(newCallFrame);
-
-    newCallFrame.join({ url: room });
-    console.log("Me uno");
-    // handleUpdateChecking();
-
-    const leaveCall = () => {
-      console.log("Se llama leaveCall");
-      setRoom(null);
-      setCallFrame(null);
-      refreshData();
-      callFrame.destroy();
-    };
-
-    newCallFrame.on("left-meeting", leaveCall);
-    console.log("Termina el ciclo");
-  }, [room, setCallFrame]);
+        theme: {
+          colors: {
+            accent: "#4D22B3",
+            accentText: "#FFFFFF",
+            background: "#1F1D36",
+            backgroundAccent: "#311673",
+            baseText: "#FFFFFF",
+            border: "#574770",
+            mainAreaBg: "#000000",
+            mainAreaBgAccent: "#333333",
+            mainAreaText: "#FFFFFF",
+            supportiveText: "#808080",
+          },
+        },
+        userName: session?.user.firstName!,
+        showLeaveButton: true,
+        showFullscreenButton: false,
+      });
+      console.log("callFrame Creado, es: " + newCallFrame);
+      setCallFrame(newCallFrame);
+  
+      newCallFrame.join({ url: room });
+      console.log("Me uno");
+      // handleUpdateChecking();
+  
+      const leaveCall = () => {
+        console.log("Se llama leaveCall");
+        setRoom(null);
+        setCallFrame(undefined);
+        refreshData();
+        callFrame.destroy();
+      };
+  
+      newCallFrame.on("left-meeting", leaveCall);
+      console.log("Termina el ciclo");
+    } else {
+        console.log("Sí existe el bicho");
+        await oldCall.destroy();
+        console.log("Destruido");
+    }
+  }
 
   /**
    * Initiate Daily iframe creation on component render if it doesn't already exist
    */
   useEffect(() => {
-    console.log("callFrame es: " + callFrame);
-    if (callFrame) return;
-    console.log("callFrame no existe: " + callFrame);
+    // console.log("callFrame es: " + callFrame);
+    if (status === "authenticated" && !callFrame) {
+    // console.log("callFrame no existe: " + callFrame);
+    console.log("username es:" + session?.user.firstName);
     createAndJoinCall();
     console.log("Ya hice lo mío");
-  }, [callFrame, createAndJoinCall]);
+    }
+  }, [callFrame, createAndJoinCall, userName]);
 
   return (
     <Box>
