3a22048ab4deabe91cda743d030818c72c45db07
Samuel Riascos Prieto <samuel.riascos@javerianacali.edu.co>
2024-05-02 16:56:17 -0500

    Versión inicial del CSS del componente NotesDrawer

--- src/components/notes/drawer panels/EditNotePanel.tsx
+++ src/components/notes/drawer panels/EditNotePanel.tsx
@@ -1,8 +1,16 @@
+import { Button } from "@mui/material";
 import { useState } from "react";
 import ReactQuill from "react-quill";
 
 export default function EditNotePanel() {
-    const [value, setValue] = useState('');
+  const [value, setValue] = useState("<h1>Esto es Esparta	</h1><ol><li>Dijeron los espartanos</li><li>Luego, todos murieron</li></ol><ul><li>Debe mejorar</li><li>alñskd</li></ul><p><br></p>");
 
-  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
+  return (
+    <div className="edit-note-panel">
+      <ReactQuill theme="snow" value={value} onChange={setValue} />
+      <Button id="save-note-button" color="secondary" onClick={() => console.log(value)}>Guardar</Button>
+      {/* <div dangerouslySetInnerHTML={{ __html: value }}/> */}
+      <div/>
+    </div>
+  );
 }
