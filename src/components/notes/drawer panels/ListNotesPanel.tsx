31e07b1b8338f97c2edd552def5386bde38d0e8f
Samuel Riascos Prieto <samuel.riascos@javerianacali.edu.co>
2024-05-03 17:40:17 -0500

    Avances remotos

--- src/components/notes/drawer panels/ListNotesPanel.tsx
+++ src/components/notes/drawer panels/ListNotesPanel.tsx
@@ -1,7 +1,17 @@
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+
 export default function ListNotesPanel() {
-    return(
-        <h6>
-            Aquí van las notas guardadas
+    return (
+        <h6 id="list-notes-panel">
+            <List>
+                {Array(4).fill(null).map(() =>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemText primary="título" secondary="creado el ta ta tá" />
+                        </ListItemButton>
+                    </ListItem>
+                )}
+            </List>
         </h6>
     )
 }
