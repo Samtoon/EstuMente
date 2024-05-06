6c4bee02e85ff048ca39229de5a85aa01bddc205
Samuel Riascos Prieto <samuel.riascos@correounivalle.edu.co>
2024-05-05 20:32:32 -0500

    Se completa la modal de notas
    1- Se actualiza la variable de entorno NEXTAUTH_SECRET en el archivo .env
    2- Se trasladan los componentes Draggable y Transition de la ruta app/pruebas a components/notes
    3- Se eliminan las dependencias innecesarias de la página pruebas/page.tsx
    4- Se añade una tercera pestaña a la modal de notas, que permite visualizar y editar notas guardadas
    5- Se elimina el backdrop de la modal de notas, permitiendo la interacción con el resto de la pantalla
    6- Ahora, notas solo se puede cerrar mediante el botón integrado de minimizar
    7- Se modifica la lógica de estados entre la modal de notas y sus componentes, para permitir que se puedan comunicar entre sí
    y la lista de notas se actualice automáticamente conforme se añaden o editan notas
    8- Se realizan ajustes al componente EditNotePanel.tsx con el fin de que pueda reusarse para ver y editar notas existentes
    9- Se crea la pestaña ListNotesPanel.tsx para la modal de notas, que permite listar y filtrar por título o fecha las notas escritas por un practicante
    10- Se crea el componente SearchField, la barra de búsqueda de la pestaña ListNotesPanel.tsx
    11- Se añaden las functiones getNotesByTitle, getNotesByDate y updateNote a noteDao.ts
    12- Se añade un enum que almacena todos los posibles filtros para las notas
    13- Se hacen pequeños ajustes a notes-drawer.css y light-theme.ts
    14- Se modifica la función saveNote de actions.ts para que permita actualizar o crear una nueva nota dependiendo si la nota que recibe como parámetro
    tiene un _id o no
    15- Se crea la función filterNotes en el archivo actions.ts, delegando la tarea de filtrar las notas a la base de datos

--- src/app/pruebas/page.tsx
+++ src/app/pruebas/page.tsx
@@ -1,45 +1,21 @@
 "use client"
 import { PsychologistLayout } from "@/components/layout/PsychologistLayout";
 import NotesDrawer from "@/components/notes/NotesDrawer";
-import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
-import { Button, Dialog, DialogContent, DialogTitle, Paper, PaperProps, Slide } from "@mui/material";
-import { TransitionProps } from "@mui/material/transitions";
-import React from "react";
-import { ReactNode, useState } from "react";
-import Draggable from "react-draggable"
-import SlideTransition from "./Transition";
+import { Button } from "@mui/material";
+import { useState } from "react";
 
-interface Props {
-
-}
-
-// function Transition({ children } : { children: JSX.Element }) {
-//     return(
-//         <Slide direction="up">
-//             {children}
-//         </Slide>
-//     )
-// }
-
-
-
-
-
-
+interface Props {}
 export default function Pruebas(props: Props) {
     const [open, setOpen] = useState(false);
-    const [value, setValue] = useState('');
     console.log("Open es: " + open);
     return (
         <PsychologistLayout title="Pruebas" pageDescription="Pruebas">
             <h1>Pruebas</h1>
-
             <Button color="secondary" onClick={() => setOpen(true)}>
                 Abrir Notas
             </Button>
             <NotesDrawer open={open} handleClose={() => setOpen(false)}/>
-            <ReactQuill theme="snow" value={value} onChange={setValue} />
         </PsychologistLayout>
     )
 }
