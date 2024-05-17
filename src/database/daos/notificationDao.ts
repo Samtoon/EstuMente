e8d118c2f4841acf7d0757e2cd4885d47b611b17
Samuel Riascos Prieto <samuel.riascos@correounivalle.edu.co>
2024-05-17 16:25:49 -0500

    Se añaden las promesas await connect al DAO de Notificaciones

--- src/database/daos/notificationDao.ts
+++ src/database/daos/notificationDao.ts
@@ -1,18 +1,22 @@
 import { INotification } from "@/interfaces/INotification";
 import Notification from "../models/Notification";
+import { connect } from "../connection";
 
 export async function getNotificationsByUser(user: string) {
+    await connect();
     console.log("Llaman al notifications");
     const notifications = await Notification.find({ user: user }).sort({createdAt: -1}).lean();
     return notifications;
 }
 
 export async function createNotification(notification: INotification) {
+    await connect();
     const result = await Notification.create(notification);
     return Boolean(result);
 }
 
 export async function deleteNotificationById(id: string) {
+    await connect();
     const result = await Notification.deleteOne({ _id: id });
     return Boolean(result.deletedCount);
 }
